907ed666042cda37f2e2303aa80e5293e46fca90
EC2 Default User <ec2-user@ip-172-31-34-48.ap-northeast-2.compute.internal>
2022-02-28 07:43:56 +0000

    example_1.py

---
+++ example_1.py
@@ -0,0 +1,10 @@
+#! /usr/bin/env python
+
+import numpy as np
+
+array_1 = np.array([2,3,5])
+array_2 = np.array([7,11,13])
+
+array_1 + array_2
+
+print(array_1 @ array_2)
---
+++ practice_1.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python
+
+
+for i in range(3,7):
+    print("There are", i, "chestnuts at your place!")
+print("Hello World!")
